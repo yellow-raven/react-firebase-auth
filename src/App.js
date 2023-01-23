968ff989b0a3ceaa52be83a98497df539c164fa2
Francois Lebas <francois@constructionfuture.org>
2023-01-23 18:21:54 -0500

    login with firebase-auth directly in front

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
